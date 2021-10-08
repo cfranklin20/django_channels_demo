27584b615105b287047b2072fc61ed0141eb8af6
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2021-10-07 17:43:23 -0700

    Adding files for django demo

---
+++ chat/urls.py
@@ -0,0 +1,11 @@
+# chat/urls.py
+from functools import update_wrapper
+from django.conf.urls import url
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
+]
